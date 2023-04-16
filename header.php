c3250e38ebe2c93894513c7337f4ed072a9fd35a
Tongguang Watanabe Xia <ghbq674521@163.com>
2023-04-15 20:31:49 -0500

    feat: adding safari theme color

--- header.php
+++ header.php
@@ -15,6 +15,8 @@
   <link rel="apple-touch-icon" sizes="152x152" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-152x152.png" />
   <link rel="apple-touch-icon" sizes="180x180" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-180x180.png" />
 
+  <meta name="theme-color" content="#fff9b8">
+
   <meta charset="<?php $this->options->charset(); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
   <meta name="renderer" content="webkit">
