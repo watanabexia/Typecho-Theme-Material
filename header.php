1436e1d8121bf88cf30848c05fcdfed18f694fcf
Tongguang Watanabe Xia <ghbq674521@163.com>
2023-04-15 20:05:17 -0500

    feat: apple-touch-icon

--- header.php
+++ header.php
@@ -5,6 +5,16 @@
   <link rel="shortcut icon" href="<?php $this->options->themeUrl('')?>img/autumn.png" type="image/png" />
   <link rel="bookmark" href="<?php $this->options->themeUrl('')?>img/autumn.png" type="image/png" />
 
+  <link rel="apple-touch-icon" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon.png" />
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php $this->options->themeUrl('')?>img/apple-touch-icon-180x180.png" />
+
   <meta charset="<?php $this->options->charset(); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
   <meta name="renderer" content="webkit">
